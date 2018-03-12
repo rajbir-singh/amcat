880ce54332b520da10a69303af40fcf4cfb10987
hurb <rajbirs799@gmail.com>
2018-03-12 08:46:49 +0530

    view changes

--- amcatCandidateDetails/urls.py
+++ amcatCandidateDetails/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'amcatCandidateDetails'
 urlpatterns = [
-    url(r'^candetails/(?P<orderId>\d+)/(?P<count>\d+)/$', views.scrapDetails)
+    url(r'^admitCards/(?P<orderId>\d+)/(?P<count>\d+)/$', views.scrapDetails)
     # url(r'^candetails/(?P<orderId>\d+)/(?P<count>\d+)/$', views.ScrapDetails.as_view())
 ]
